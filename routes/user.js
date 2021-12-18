f4d94027d802d3dba9b20adcf365a0318f27225c
Ismail Brkic <brkicismail@gmail.com>
2021-12-18 15:10:52 +0100

    User authentication and authorization implemented.

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup, signin, signout } = require("../controllers/user");
-const { userSignupValidator } = require("../validator");
+const { userById } = require("../controllers/user");
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 
-router.post("/signup", userSignupValidator, signup);
-router.post("/signin", signin);
-router.get("/signout", signout);
+router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
+  res.json({ user: req.profile });
+});
+
+// run this middleware whenever there is userId in query
+router.param("userId", userById);
 
 module.exports = router;
